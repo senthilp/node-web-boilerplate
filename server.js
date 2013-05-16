c213bc73cbcd2b9e0d186f1c95b4893a16e71423
senthilp <senthil.padmanabhan@yahoo.com>
2013-05-16 11:51:06 -0700

    using express as defaul

--- server.js
+++ server.js
@@ -4,16 +4,21 @@ var app = require('./app.js')
     , Cluster = require('cluster2')
 	, conf = require('config');
 
+app.listen(conf.server.port);
+console.log('Server started at port ' + conf.server.port);
+
 //initialize cluster2 instance
-var cluster = new Cluster({
-    port : conf.server.port,
-    monPort : conf.server.monPort,
-    ecv : {
-    	path : conf.server.ecvPath,
-    	control : conf.server.ecvControl
-    }
-});
-cluster.listen(function(cb) {
-    cb(app);
-    console.log('Server started at port ' + conf.server.port);
-});
+if(false){
+    var cluster = new Cluster({
+        port : conf.server.port,
+        monPort : conf.server.monPort,
+        ecv : {
+        	path : conf.server.ecvPath,
+        	control : conf.server.ecvControl
+        }
+    });
+    cluster.listen(function(cb) {
+        cb(app);
+        console.log('Server started at port ' + conf.server.port);
+    });
+}
