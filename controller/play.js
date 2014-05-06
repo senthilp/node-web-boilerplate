8ce8973162325d3a2ebcf16e443cc306c78d727e
senthilp <senthil.padmanabhan@yahoo.com>
2014-05-06 15:40:50 -0700

    Experimenting with react

--- controller/play.js
+++ controller/play.js
@@ -1,5 +1,6 @@
 var expressUtil = require('../libs/expressUtil');
 
+
 module.exports = function(req, res) {
-	expressUtil.send(req, res, 'play.dust', {});
+	expressUtil.send(req, res, 'play.dust', {"searchQuery": "Soccer"});
 };
