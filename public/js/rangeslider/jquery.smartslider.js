ac39e8e9f2c26382b04871cc020ca0907da0be70
senthilp <senthil.padmanabhan@yahoo.com>
2014-05-08 15:08:33 -0700

    Making the smartslider ebay specific

--- public/js/rangeslider/jquery.smartslider.js
+++ public/js/rangeslider/jquery.smartslider.js
@@ -1,32 +1,16 @@
 (function($) {
 	 
-	$.tools = $.tools || {version: '@VERSION'};
-	 
-	var tool;
-	
-	tool = $.tools.rangeinput = {
-        
-		conf: {
-			min: 0,
-			max: 100,		// as defined in the standard
-			step: 'any',	// granularity of the value. a non-zero float or int (or "any")
-			steps: 0,
-			value: 0,
-			precision: undefined,
-			vertical: 0,
-			keyboard: true,
-			progress: false,
-			speed: 100,
-			
-			// set to null if not needed
-			css: {
-				input: 'range',
-				slider: 'slider',
-				progress: 'progress',
-				handle: 'handle'
-			}
-
-		}
+	var defaultConf = {
+		min: 0,
+		max: 100,		// as defined in the standard
+		step: 'any',	// granularity of the value. a non-zero float or int (or "any")
+		steps: 0,
+		value: 0,
+		precision: undefined,
+		vertical: 0,
+		keyboard: true,
+		progress: false,
+		speed: 100
 	};
 	
 //{{{ fn.drag
@@ -48,7 +32,7 @@
 	*/
 	var doc, draggable;
 	
-	$.fn.drag = function(conf) {
+	$.fn.draglight = function(conf) {
 		
 		// disable IE specialities
 		document.ondragstart = function () { return false; };
@@ -120,70 +104,45 @@
 		return s && s.width && parseInt(s.width, 10);
 	}
 	
-	function hasEvent(el) {
-		var e = el.data("events");
-		return e && e.onSlide;
-	}
-	
-	function RangeInput(input, conf) {
+	function SmartSlider($elem, conf) {
 		
 		// private variables
 		var self = this,
-			css = conf.css,
-			root = $("<div><div/><a href='#'/></div>").data("rangeinput", self),
+			$handle = $elem.find("a"),
 			vertical,
 			value,			// current value
+			prevValue,      // previous value
 			origo,			// handle's start point
 			len,				// length of the range
-			pos;				// current position of the handle		
-		 
-		// create range	 
-		input.before(root);
-		
-		var handle = root.addClass(css.slider).find("a").addClass(css.handle),
-			progress = root.find("div").addClass(css.progress);
-		
-		// get (HTML5) attributes into configuration
-		$.each("min,max,step,value".split(","), function(i, key) {
-			var val = input.attr(key);
-			if (parseFloat(val)) {
-				conf[key] = parseFloat(val, 10);
-			}
-		});
-		
-		var range = conf.max - conf.min,
-			step = conf.step === 'any'?0 : conf.step,
-			precision = conf.precision;
-			 
-		if (precision === undefined) {
-			precision = step.toString().split(".");
-			precision = precision.length === 2 ? precision[1].length : 0;
-		}
-		
-		// Replace built-in range input (type attribute cannot be changed)
-		if (input.attr("type") === 'range') {
-			var def = input.clone().wrap("<div/>").parent().html(),
-				clone = $(def.replace(/type/i, "type=text data-orig-type"));
-				 
-			clone.val(conf.value);
-			input.replaceWith(clone);
-			input = clone;
-		}
-		
-		input.addClass(css.input);
-			 
-		var fire = $(self).add(input), fireOnSlide = true;
+			pos,				// current position of the handle		
+			range,
+			step,
+			precision;
 
-		
+		function triggerChange(val) {
+			$elem.trigger('smartslider.change', val);
+		}
+
+		function triggerSlide(val) {
+			$elem.trigger('smartslider.slide', val);
+		}
+
+		function getPosition($el, pageX, pageY) {
+			var fix = vertical ? $el.height() / 2 : $el.width() / 2,
+				x= vertical ? len-origo-fix + pageY  : pageX -origo -fix;
+			return x;
+		}
+
 		/**
 			The flesh and bone of this tool. All sliding is routed trough this.
 			
 			@param evt types include: click, keydown, blur and api (setValue call)
 			@param isSetValue when called trough setValue() call (keydown, blur, api)
+			@param bChangeEvent boolean flag to trigger a change event			
 			
 			vertical configuration gives additional complexity. 
 		 */
-		function slide(evt, x, val, isSetValue) {
+		function slide(evt, x, val, isSetValue, bChangeEvent) {
 
 			// calculate value based on slide position
 			if (val === undefined) {
@@ -227,32 +186,22 @@
 			// precision
 			val = round(val, precision);
 			
-			// onSlide
+			// smartslider.slide
 			var isClick = evt.type === "click";
-			if (fireOnSlide && value !== undefined && !isClick) {
-				evt.type = "onSlide";
-				fire.trigger(evt, [val, x]);
-				if (evt.isDefaultPrevented()) { return self; }
+			if (val !== value && !isClick) {
+				triggerSlide([val, x]);
 			}
 			
 			// speed & callback
 			var speed = isClick ? conf.speed : 0,
-				callback = isClick ? function()  {
-					evt.type = "change";
-					fire.trigger(evt, [val]);
+				callback = (isClick || bChangeEvent) && val !== value ? function()  {
+					triggerChange(val);
 				} : null;
 			
 			if (vertical) {
-				handle.animate({top: x}, speed, callback);
-				if (conf.progress) {
-					progress.animate({height: len - x + handle.height() / 2}, speed);
-				}
-				
+				$handle.animate({top: x}, speed, callback);
 			} else {
-				handle.animate({left: x}, speed, callback);
-				if (conf.progress) {
-					progress.animate({width: x + handle.width() / 2}, speed);
-				}
+				$handle.animate({left: x}, speed, callback);
 			}
 			
 			// store current value
@@ -260,11 +209,27 @@
 			pos = x;
 			
 			// se input field's value
-			input.val(val);
+			$elem.data('value', val);
 
 			return self;
 		}
 		
+		// get (HTML5) attributes into configuration
+		$.each("min,max,step,value".split(","), function(i, key) {
+			var val = $elem.data(key);
+			if (parseFloat(val)) {
+				conf[key] = parseFloat(val, 10);
+			}
+		});
+		
+		range = conf.max - conf.min;
+		step = conf.step === 'any'?0 : conf.step;
+		precision = conf.precision;
+			 
+		if (precision === undefined) {
+			precision = step.toString().split(".");
+			precision = precision.length === 2 ? precision[1].length : 0;
+		}
 		
 		$.extend(self, {
 			
@@ -272,31 +237,27 @@
 				return value;
 			},
 			
-			setValue: function(val, e) {
+			setValue: function(val, e, bChangeEvent) {
 				init();
-				return slide(e || $.Event("api"), undefined, val, true);
+				return slide(e || $.Event("api"), undefined, val, true, bChangeEvent);
 			},
 			
 			getConf: function() {
 				return conf;
 			},
 			
-			getProgress: function() {
-				return progress;
-			},
-
 			getHandle: function() {
-				return handle;
+				return $handle;
 			},
 			
-			getInput: function() {
-				return input;
+			getElem: function() {
+				return $elem;
 			},
 				
 			step: function(am, e) {
 				e = e || $.Event();
 				var step = conf.step === 'any' ? 1 : conf.step;
-				self.setValue(value + step * (am || 1), e);
+				self.setValue(value + step * (am || 1), e, true);
 			},
 			
 			// HTML5 compatible name
@@ -310,64 +271,49 @@
 			}
 			
 		});
-		
-		// callbacks
-		$.each("onSlide,change".split(","), function(i, name) {
-				
-			// from configuration
-			if ($.isFunction(conf[name]))  {
-				$(self).on(name, conf[name]);
-			}
-			
-			// API methods
-			self[name] = function(fn) {
-				if (fn) { $(self).on(name, fn); }
-				return self;
-			};
-		});
-			
 
 		// dragging
-		handle.drag({drag: false}).on("dragStart", function() {
-		
+		$handle.draglight({drag: false}).on("dragStart", function() {
 			/* do some pre- calculations for seek() function. improves performance */
 			init();
-			
-			// avoid redundant event triggering (= heavy stuff)
-			fireOnSlide = hasEvent($(self)) || hasEvent(input);
-			
-				
 		}).on("drag", function(e, y, x) {
-			
-			if (input.is(":disabled")) { return false; }
+			if ($elem.is(":disabled")) { return false; }
 			slide(e, vertical ? y : x);
-			
 		}).on("dragEnd", function(e) {
 			if (!e.isDefaultPrevented()) {
 				e.type = "change";
-				fire.trigger(e, [value]);
+				$elem.trigger(e, [value]);
 			}
 			
 		}).click(function(e) {
 			return e.preventDefault();
 		});
-		
+
 		// clicking
-		root.click(function(e) {
-			if (input.is(":disabled") || e.target === handle[0]) {
+		$elem.click(function(e) {
+			if ($elem.is(":disabled") || e.target === $handle[0]) {
 				return e.preventDefault();
 			}
 			init();
-			var fix = vertical ? handle.height() / 2 : handle.width() / 2;
-			slide(e, vertical ? len-origo-fix + e.pageY  : e.pageX -origo -fix);
+			slide(e, getPosition($handle, e.pageX, e.pageY));
+		});
+
+		// mousedown on handle
+		$handle.on('mousedown', function() {
+			prevValue = value;
+		});
+
+		// mouseup on handle
+		$handle.on('mouseup', function() {
+			if(value !== prevValue) {
+				triggerChange(value);
+			}
 		});
 
 		if (conf.keyboard) {
 			
-			input.keydown(function(e) {
-		
-				if (input.attr("readonly")) { return; }
-				
+			doc.on('keydown', function(e) {
+					
 				var key = e.keyCode,
 					up = $([75, 76, 38, 33, 39]).index(key) !== -1,
 					down = $([74, 72, 40, 34, 37]).index(key) !== -1;
@@ -376,40 +322,31 @@
 				
 					// UP:	k=75, l=76, up=38, pageup=33, right=39			
 					if (up) {
-						self.step(key === 33 ? 10 : 1, e);
+						self.step(key === 33 ? conf.max : 1, e);
 						
 					// DOWN:	j=74, h=72, down=40, pagedown=34, left=37
 					} else if (down) {
-						self.step(key === 34 ? -10 : -1, e);
+						self.step(key === 34 ? -conf.max : -1, e);
 					}
 					return e.preventDefault();
 				}
 			});
 		}
-		
-		
-		input.blur(function(e) {
-			var val = $(this).val();
-			if (val !== value) {
-				self.setValue(val, e);
-			}
-		});
-		
-		
+			
 		// HTML5 DOM methods
-		$.extend(input[0], { stepUp: self.stepUp, stepDown: self.stepDown});
+		$.extend($elem[0], { stepUp: self.stepUp, stepDown: self.stepDown});
 		
 		
 		// calculate all dimension related stuff
 		function init() {
-			vertical = conf.vertical || dim(root, "height") > dim(root, "width");
+			vertical = conf.vertical || dim($elem, "height") > dim($elem, "width");
 		 
 			if (vertical) {
-				len = dim(root, "height") - dim(handle, "height");
-				origo = root.offset().top + len;
+				len = dim($elem, "height") - dim($handle, "height");
+				origo = $elem.offset().top + len;
 			} else {
-				len = dim(root, "width") - dim(handle, "width");
-				origo = root.offset().left;
+				len = dim($elem, "width") - dim($handle, "width");
+				origo = $elem.offset().left;
 			}
 		}
 		
@@ -425,31 +362,17 @@
 		}
 	}
 	
-	$.expr[':'].range = function(el) {
-		var type = el.getAttribute("type");
-		return type && type === 'range' || !!$(el).filter("input").data("rangeinput");
-	};
-	
-	
 	// jQuery plugin implementation
-	$.fn.rangeinput = function(conf) {
-
-		// already installed
-		if (this.data("rangeinput")) { return this; }
+	$.fn.smartslider = function(conf) {
 		
 		// extend configuration with globals
-		conf = $.extend(true, {}, tool.conf, conf);
+		conf = $.extend(true, {}, defaultConf, conf);
 		
-		var els;
-		
-		this.each(function() {
-			var el = new RangeInput($(this), $.extend(true, {}, conf));
-			var input = el.getInput().data("rangeinput", el);
-			els = els ? els.add(input) : input;
+		return this.each(function() {
+			new SmartSlider($(this), $.extend(true, {}, conf));
 		});
-		
-		return els ? els : this;
 	};
 	
+	$('.smartslider[data-init="true"]').smartslider();
 	
 }) (jQuery);
