793e0b096ee2cc4d0121223a6818b4cab299a00c
senthilp <senthil.padmanabhan@yahoo.com>
2014-10-01 13:30:36 -0700

    Fixing the issue when deduping the namespace

--- public/js/spf/_event-init.js
+++ public/js/spf/_event-init.js
@@ -5,15 +5,29 @@
 		$neo = $('noscript.neo');
 
 	if($neo.length && $neo.data('eventing')) {
-		var reservedNamespace = ($neo.data('reservedns') || DEFAULT_RESERVED_NAMESPACE).replace(/\s/g, '');
-		// Check if the DEFAULT_RESERVED_NAMESPACE is part of the passed namespaces
-		if(!new RegExp(DEFAULT_RESERVED_NAMESPACE + '(\\.|,|$)').test(reservedNamespace)) {
-			reservedNamespace += ',' + DEFAULT_RESERVED_NAMESPACE;
-		} 
+		var reservedNamespaceList = $neo.data('reservedns') || [],
+			reservedNamespace = [],
+			i,
+			l;
+		if(reservedNamespaceList.length) {
+			reservedNamespaceList = reservedNamespaceList.replace(/\s/g, '').split(',');
+		}
+		for(i = 0, l = reservedNamespaceList.length;i < l; i++) {
+			// Check if the DEFAULT_RESERVED_NAMESPACE is part of the passed namespaces - RegExp "(gh(\.|,|$)|ghn(\.|,|$))"		
+			if(!new RegExp('(' + DEFAULT_RESERVED_NAMESPACE.concat(',')
+															.split(',')
+															.join('(\\.|,|$)|')
+															.slice(0, -1)
+															.concat(')')).test(reservedNamespaceList[i])) {
+				reservedNamespace.push(reservedNamespaceList[i]);
+			} 
+		}
+		// Finally add default to the reserved namespace
+		reservedNamespace.push(DEFAULT_RESERVED_NAMESPACE);
 		$._eventinit({
 			"pageContainer": $neo.data('container'),
 			"delegateToPage": $neo.data('delegatetopage'),
-			"reservedNamespace": reservedNamespace
+			"reservedNamespace": reservedNamespace.join(',')
 		});		
 	}
 })(window.jQuery);
