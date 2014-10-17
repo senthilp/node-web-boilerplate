e5f5347ef2f1de735c97cbeafb6998ac79f80dbe
senthilp <senthil.padmanabhan@yahoo.com>
2014-10-17 10:23:00 -0700

    Adding init to package.json

--- public/js/spf/spf-init.js
+++ public/js/spf/spf-init.js
@@ -15,7 +15,7 @@
 				var	itemUrlPattern = /https?:\/\/[^\/]+\.ebay\.[^\/]+\/(soc\/)?itm\//,
 					cgiUrlPattern = /https?:\/\/[^\/]+\.ebay\.[^\/]+\/ws\/eBayISAPI.dll\?ViewItem&item=(\d+)/;
 
-				return itemUrlPattern.test(url) || cgiUrlPattern.test(url);
+				return !itemUrlPattern.test(url) || cgiUrlPattern.test(url);
 			}
 		});		
 	}
